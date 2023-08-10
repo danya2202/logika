54d954341357e5ea4985da7a975d12d2a71f52c8
danya2202 <134725632+danya2202@users.noreply.github.com>
2023-08-10 17:42:03 +0300

    Add files via upload

---
+++ logika/m1/u3/словники/class.py
@@ -0,0 +1,14 @@
+class lyduna():
+    def __init__(self, rist, vaga, imya):
+        self.ris = rist
+        self.vaga = vaga
+        self.imya = imya
+
+    def duxatu(self):
+        print(self.imya, "дихає")
+
+h1 = lyduna(192, 67, 'Gleb')
+h1.duxatu()
+
+
+
