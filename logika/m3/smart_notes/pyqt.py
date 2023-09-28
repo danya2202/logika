f2fdb903f85512b766cc1efb9acb46fa8ecd8c10
danya2202 <134725632+danya2202@users.noreply.github.com>
2023-09-28 20:02:08 +0300

    Add files via upload

--- logika/m3/smart_notes/pyqt.py
+++ logika/m3/smart_notes/pyqt.py
@@ -7,6 +7,9 @@ from PyQt5.QtWidgets import (
 
 import json
 
+def writeToFile():
+    with open('notes.json', 'w', encoding='utf8') as file:
+        json.dump(notes, file, ensure_ascii=False, sort_keys=True, indent=4)
 app = QApplication([])
 
 window = QWidget()
@@ -16,55 +19,104 @@ field_text = QTextEdit()
 
 lb_notes = QLabel('')
 
-btn_note_create = QPushButton()
-btn_note_delete = QPushButton()
-btn_note_save = QPushButton()
+lst_list = QListWidget()
+
+btn_note_create = QPushButton('створити')
+btn_note_delete = QPushButton('видалити')
+btn_note_save = QPushButton('зберегти')
 
 lb_teg = QLabel('')
 
 lst_tags = QListWidget()
 
-col2 = QVBoxLayout()
+lst_text = QTextEdit
 
 layout_notes = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
 
-lst_notes = QLabel()
+layout_notes.addLayout(col1, stretch=2)
+layout_notes.addLayout(col2, stretch=1)
 
+col1.addWidget(field_text)
 
+lst_notes = QListWidget()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-col2
+btn_tag_seacrh = QPushButton('шукати')
+btn_tag_add = QPushButton('додати')
+btn_tag_delete = QPushButton('видалити')
 
 row1 = QHBoxLayout()
+row2 = QHBoxLayout()
 row1.addWidget(btn_note_create)
 row1.addWidget(btn_note_delete)
 
+row2
+
+
+
+
+
 col2.addLayout(row1)
-col2.addWidget(btn_note_save)
+col2.addLayout(row2)
+col2.addWidget(btn_tag_seacrh)
+
 
 def show_notes():
     key = lst_notes.selectedItems()[0].text()
-    
+    field_text.setText(notes[key]['текст'])
 
 
-# lst_notes.itemClick.connect(show_notes)
+    lst_tags.clear()
+    lst_tags.addItems(notes[key]['теги'])
 
-with open('smart_notes/notes.json', 'r', encoding='utf8') as file:
+with open('notes.json', 'r', encoding='utf8') as file:
     notes = json.load(file)
+
+def show_note():
+    key = lst_notes.currentItem().text()
+
+    print(notes[key]['текст'])
+
+def add_note():
+    note_name, ok = QInputDialog.getText(window, 'додаток', 'Назва замітки')
+
+    if note_name and ok:
+        
+        notes[note_name] = {"текст": '', "теги": []}
+        lst_notes.addItem(note_name)
+
+def save_note():
+    if lst_notes.currentItem():
+        key = lst_notes.currentItem().text()
+        notes[key]['текст'] = field_text.toPlainText()
+
+        
+        writeToFile()
+        
+    else:
+        print("no")
+        
+def del_note():
+        if lst_notes.currentItem():
+            key = lst_notes.currentItem().text()
+            del notes[key]
+            
+            
+            
+            field_text.clear()
+            lst_notes.clear()
+            lst_tags.clear()
+            
+            writeToFile()
+
+lst_notes.itemClicked.connect(show_notes)   
+
+btn_note_create.clicked.connect(add_note)
+btn_note_save.clicked.connect(save_note)
+btn_note_delete.clicked.connect(del_note)
+lst_notes.addItems(notes)
+
 
 window.setLayout(layout_notes)
 window.show()
