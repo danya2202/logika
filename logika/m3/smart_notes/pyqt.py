d2b175ca1f6bc2d2cf77056a3baf6e748e9d5999
danya2202 <134725632+danya2202@users.noreply.github.com>
2023-10-05 18:23:04 +0300

    Add files via upload

--- logika/m3/smart_notes/pyqt.py
+++ logika/m3/smart_notes/pyqt.py
@@ -17,7 +17,7 @@ window.show()
 
 field_text = QTextEdit()
 
-lb_notes = QLabel('')
+lb_notes = QLabel('Список заміток')
 
 lst_list = QListWidget()
 
@@ -25,11 +25,6 @@ btn_note_create = QPushButton('створити')
 btn_note_delete = QPushButton('видалити')
 btn_note_save = QPushButton('зберегти')
 
-lb_teg = QLabel('')
-
-
-
-lst_text = QListWidget()
 
 layout_notes = QHBoxLayout()
 col1 = QVBoxLayout()
@@ -40,13 +35,12 @@ layout_notes.addLayout(col2, stretch=1)
 
 col1.addWidget(field_text)
 
-lst_notes = QListWidget()
 
 btn_tag_seacrh = QPushButton('шукати')
 btn_tag_add = QPushButton('додати')
 btn_tag_unpin = QPushButton('відкріпити')
 
-lb_tags = QLabel('')
+lb_tags = QLabel('Список тегів')
 lst_tags = QListWidget()
 field_tag = QLineEdit()
 
@@ -54,27 +48,29 @@ row1 = QHBoxLayout()
 row2 = QHBoxLayout()
 
 
+col2.addWidget(lb_notes)
+col2.addWidget(lst_list)
 
-col2.addWidget(lst_text)
 row1.addWidget(btn_note_create)
 row1.addWidget(btn_note_delete)
 row1.addWidget(btn_note_save)
-
-row2.addWidget(lst_tags)
-
-
-
-
 col2.addLayout(row1)
-col2.addLayout(row2)
-col2.addWidget(field_tag)
-col2.addWidget(btn_tag_seacrh)
-col2.addWidget(btn_tag_unpin)
-col2.addWidget(btn_tag_add)
+col2.addWidget(lb_tags)
+col2.addWidget(lst_tags)
 
+
+
+
+
+
+col2.addWidget(field_tag)
+row2.addWidget(btn_tag_seacrh)
+row2.addWidget(btn_tag_unpin)
+row2.addWidget(btn_tag_add)
+col2.addLayout(row2)
 
 def show_notes():
-    key = lst_notes.selectedItems()[0].text()
+    key = lst_list.selectedItems()[0].text()
     field_text.setText(notes[key]['текст'])
 
 
@@ -85,7 +81,7 @@ with open('notes.json', 'r', encoding='utf8') as file:
     notes = json.load(file)
 
 def show_note():
-    key = lst_notes.currentItem().text()
+    key = lst_list.currentItem().text()
 
     print(notes[key]['текст'])
 
@@ -95,11 +91,11 @@ def add_note():
     if note_name and ok:
         
         notes[note_name] = {"текст": '', "теги": []}
-        lst_notes.addItem(note_name)
+        lst_list.addItem(note_name)
 
 def save_note():
-    if lst_notes.currentItem():
-        key = lst_notes.currentItem().text()
+    if lst_list.currentItem():
+        key = lst_list.currentItem().text()
         notes[key]['текст'] = field_text.toPlainText()
 
         
@@ -109,24 +105,24 @@ def save_note():
         print("no")
         
 def del_note():
-        if lst_notes.currentItem():
-            key = lst_notes.currentItem().text()
+        if lst_list.currentItem():
+            key = lst_list.currentItem().text()
             del notes[key]
             
             
             
             field_text.clear()
-            lst_notes.clear()
+            lst_list.clear()
             lst_tags.clear()
             
             writeToFile()
 
-lst_notes.itemClicked.connect(show_notes)   
+lst_list.itemClicked.connect(show_notes)   
 
 btn_note_create.clicked.connect(add_note)
 btn_note_save.clicked.connect(save_note)
 btn_note_delete.clicked.connect(del_note)
-lst_notes.addItems(notes)
+lst_list.addItems(notes)
 
 
 window.setLayout(layout_notes)
